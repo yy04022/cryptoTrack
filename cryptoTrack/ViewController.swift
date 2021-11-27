37d23d816a85f22c3942099fbb6c61f0caa80d01
Jinyu Wang <ellie5201314@gmail.com>
2021-11-27 16:24:07 -0500

    Initial Commit

---
+++ cryptoTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cryptoTrack
+//
+//  Created by Yu Jin on 11/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
