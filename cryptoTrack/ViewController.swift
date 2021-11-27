f8e0e317299087f499f2a0b14f5ce8564c4a8d4a
Jinyu Wang <ellie5201314@gmail.com>
2021-11-27 18:05:05 -0500

    class exist

--- cryptoTrack/ViewController.swift
+++ cryptoTrack/ViewController.swift
@@ -9,11 +9,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        if let symbol = textField.text {
+            getData(symbol : symbol)
+        
+        }
     }
+    
 
-
+    var url = "https://min-api.cryptocompare.com/data/price?tsyms=USD"
+    
+    func getData(symbol : String) {
+        
+        url = "\(url)&fsym=\(symbol)"
+        
+        guard let url = URL(string: url) else {return}
+            
+        let task = URLSession.shared.dataTask(with: url) { [self] data, _, error in
+            
+            guard let data = data, error == nil else {return}
+            
+            print("Data Received")
+            
+            do{
+            
+            let Result = try JSONDecoder().decode(APIResponse.self, from: data)
+                print(Result.USD)
+                
+                DispatchQueue.main.async {
+                    self.outputLabel.text = "\(Result.USD)"
+                }
+                
+                
+            }
+            
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+    }
+    
+    struct APIResponse : Codable {
+        
+        let USD : Float
+        
+    }
 }
+
+
 
